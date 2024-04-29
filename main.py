d9d462986213575d0459a0176d444e7f24dd50db
Humayun Kamal <kamalhumayun320@gmail.com>
2024-04-29 16:26:59 +0500

    rerun functionality added

--- main.py
+++ main.py
@@ -5,51 +5,68 @@ from selenium import webdriver
 # time for sleeping the execution (code)
 import time
 # datetime to get latest time
-from datetime import datetime
+from datetime import datetime, timedelta
 # Selenium_wire for username and password proxy configuration
-from seleniumwire import webdriver
+# from seleniumwire import webdriver
+# for importing consts from .env
+from dotenv import load_dotenv
 
-WEBSITE_LINK = 'https://myip.ms/'
+# Load environment variables from .env file
+load_dotenv()
+
+# consts from .env
+website_link = os.getenv("WEBSITE_LINK")
+rerun_time = int(os.getenv("RERUN_TIME"))
+running_duration = int(os.getenv("RUNNING_DURATION"))
+
+# Proxy constants
+proxy_ip_auth = os.getenv("PROXY_IP_AUTH")
+proxy_username = os.getenv("PROXY_USERNAME")
+proxy_password = os.getenv("PROXY_PASSWORD")
+proxy_domain = os.getenv("PROXY_DOMAIN")
+proxy_port = os.getenv("PROXY_PORT")
+
+# Google App Password
+gmail_app_username = os.getenv("GMAIL_APP_USERNAME")
+gmail_app_password = os.getenv("GMAIL_APP_PASSWORD")
+gmail_sender = os.getenv("GMAIL_SENDER")
+gmail_receiver = os.getenv("GMAIL_RECEIVER")
 
 
-def check_website():
-    # # Config Driver
-    # # For Ip Auth Configuration
+def check_website(parent_dir):
+    # # Config Driver for Ip Auth Configuration
     # # Chrome Options
     # chrome_options = webdriver.ChromeOptions()
     # # chrome_options.add_argument("--headless") # To run scraper without in background (without open the chrome)
-    # ipconfig = f"http://proxy_domain:proxy_port"
-    # chrome_options.add_argument('--proxy-server=%s' % ipconfig)
+    # chrome_options.add_argument('--proxy-server=%s' % proxy_ip_auth)
     # driver = webdriver.Chrome(options=chrome_options)
 
-    # UserName and Password Authentication
-    proxy_options = {
-        'proxy': {
-            "http": "http://proxy_username:proxy_password@proxy_domain:proxy_port/",
-            "https": "http://proxy_username:proxy_password@proxy_domain:proxy_port/",
-            'verify_ssl': False,
-        }
-    }
-    # Config Driver
-    driver = webdriver.Chrome(seleniumwire_options=proxy_options)
+    # # UserName and Password Authentication
+    # http = f"http://{proxy_username}:{proxy_password}@{proxy_domain}:{proxy_port}"
+    # proxy_options = {
+    #     'proxy': {
+    #         "http": http,
+    #         "https": http,
+    #         'verify_ssl': False,
+    #     }
+    # }
+    # # Config Driver
+    # driver = webdriver.Chrome(seleniumwire_options=proxy_options)
+
+    # Config Driver without proxies
+    driver = webdriver.Chrome()
 
     # Run driver
     driver.maximize_window()
 
     # Get website
-    driver.get(WEBSITE_LINK)
+    driver.get(website_link)
 
     # Selenium Code for Scrapping
     # Xpath Selection
 
     # Wait for specific time
     time.sleep(10)  # in seconds
-
-    # Specify the parent directory path where you want to save screenshots
-    parent_directory = "screenshots"
-
-    # Ensure the parent directory exists
-    os.makedirs(parent_directory, exist_ok=True)
 
     # Get the current date and time
     current_time = datetime.now()
@@ -58,7 +75,7 @@ def check_website():
     formatted_time = current_time.strftime("%Y-%m-%d_%H-%M-%S")  # String format time
 
     # Combine the directory path and the formatted time to create the full path for the screenshot
-    screenshot_path = os.path.join(parent_directory, f"screenshot_{formatted_time}.png")
+    screenshot_path = os.path.join(parent_dir, f"screenshot_{formatted_time}.png")
 
     # Take a screenshot and save it to the specified directory with the current date in the filename
     driver.save_screenshot(screenshot_path)
@@ -71,5 +88,28 @@ def check_website():
     # driver.quit()
 
 
+# Specify the parent directory path where you want to save screenshots
+parent_directory = "screenshots"
+
+# Ensure the parent directory exists
+os.makedirs(parent_directory, exist_ok=True)
+
+# Calculate the end time
+end_time = datetime.now() + timedelta(seconds=running_duration)
+
+
+def main():
+    while datetime.now() < end_time:
+        try:
+            # Check website
+            check_website(parent_directory)
+            print("Main function executed successfully.")
+            time.sleep(rerun_time)  # Wait for specific minutes before running again
+
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            time.sleep(rerun_time)  # Wait for specific minutes before running again
+
+
 if __name__ == '__main__':
-    check_website()
+    main()
